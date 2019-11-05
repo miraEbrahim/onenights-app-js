ba784c0842e9f1aa0d247cb49dd7aff6c24bef87
Maryam Ebrahim <mxebrahim>
2019-11-05 14:07:48 +0000

    webpack auto name output bundles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import _ from 'lodash';
-import './css/style.css';
+import printMe from './print.js'
 import Logo from './img/mira.jpg';
+import './css/style.css';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+
   //add the image t our exsiting div
   const logo = new Image();
   logo.src = Logo;
