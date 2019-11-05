01bda4134fd93586eae1b3569e0deb832548f61b
Maryam Ebrahim <mxebrahim>
2019-11-05 10:14:34 +0000

    basic webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,9 @@
+const path = require('path');
+
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
