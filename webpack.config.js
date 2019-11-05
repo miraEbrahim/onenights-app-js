f9fbf8b3fcd128640295e8343aba48378eaf51bc
Maryam Ebrahim <mxebrahim>
2019-11-05 15:25:41 +0000

    webpack add eval-source-map

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  devtool: 'eval-source-map',
   plugins:[
     new HtmlWebpackPlugin({
       title: 'Webpack Output'
