ac97fb667db28e6d6af3e36de574e4138ffe2db6
Maryam Ebrahim <mxebrahim>
2019-11-05 10:37:43 +0000

    basic load images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './css/style.css';
+import Icon from './img/mira.jpg';
 
 function component() {
   const element = document.createElement('div');
@@ -7,6 +8,10 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+  //add the image t our exsiting div
+  const logo = new Image();
+  logo.src = Icon;
+  element.appendChild(logo);
   return element;
 }
 
