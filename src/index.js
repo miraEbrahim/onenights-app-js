861d593e433ab3ba3db826fa4d6fb2673d2c2797
Maryam Ebrahim <mxebrahim>
2019-11-06 08:53:14 +0000

    webpack add clean-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import _ from 'lodash';
 import printMe from './print.js'
-import Logo from './img/mira.jpg';
+import Logo from './img/onenight-logo.png';
 import './css/style.css';
 
 function component() {
   const element = document.createElement('div');
   const btn = document.createElement('button');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // Lodash, currently included via a script, is required for this line to works
+  element.innerHTML = _.join(['oneNights', 'Starter Pack'], ' ');
   element.classList.add('hello');
   btn.innerHTML = 'Click me and check the console';
   btn.onclick = printMe;
@@ -18,7 +18,11 @@ function component() {
   const logo = new Image();
   logo.src = Logo;
   element.appendChild(logo);
+
+
   return element;
+
+
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -12,6 +13,7 @@ module.exports = {
     contentBase: './dist',
   },
   plugins:[
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Webpack Output'
     }),
